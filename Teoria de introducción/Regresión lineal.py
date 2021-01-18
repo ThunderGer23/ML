5943c9074629844bb0b4ffa41377a54e0d0d9355
ThunderGer23 <Cuando2son1@hotmail.com>
2021-01-18 03:34:19 -0600

    Obteniendo los resultados de nuestro algoritmo implementado

--- Teoria de introducción/Regresión lineal.py
+++ Teoria de introducción/Regresión lineal.py
@@ -69,3 +69,84 @@ Existen tres tipos de errotes que pueden presentarse en nuestro sistema:
         de los modelos entrenados por su algoritmo.
 '''
 
+#Regresión lineal simple
+
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+def Init():
+    boston = datasets.load_boston()
+    print(boston)
+    print()
+    ###Y estoy viendo?###
+    input("Enter para obtener más detalles de la variable")
+    print()
+    print("Información de el dataset:")
+    print(boston.keys())
+    print()
+    input("Enter para obtener sus caracteristicas:")
+    print("Caracteristicas del dataset:")
+    print(boston.DESCR)
+    print()
+    input("Enter")
+    print("Nombres de columnas")
+    print(boston.feature_names)
+
+    ##Preparando la data de regresión lineal simple##
+
+    #Seleccionamos una columna solamente
+    x = boston.data[:, np.newaxis, 5]
+    #Definimos los datos correspondientes a las etiquetas
+    y = boston.target
+
+    plt.scatter(x, y)
+    plt.xlabel('Numero de habitaciones')
+    plt.ylabel('valor medio')
+    plt.show()
+
+    #Separamos los datos del train en entrenamiendo y prueba para probar algoritmos
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+    #Definimos el algoritmo a utilizar
+    lr = linear_model.LinearRegression()
+
+    #Entrenamiento del modelo
+    lr.fit(x_train, y_train)
+
+    #Predicción del sistema
+    y_pred = lr.predict(x_test)
+
+    #Resultados
+    print(y_test)
+    print("###################################")
+    print(y_pred)
+
+    #Grafica
+    """
+            De la siguiente grafica podemos observar que la linea roja
+            es el resultado obtenido, mientras que los puntos azules son
+            los datos que fueron utilizados para su entrenamiento. 
+            
+            Si recuerdan su curso de metodos nuemricos, para que una regresión
+            de este tipo sea satisfactoria la mayor cantidad de puntos debería
+            estar sobre la linea. Por ende podemos entender que no es un buen
+            modelo ya que los datos estan muy dispersos.    
+    """
+    plt.scatter(x_test, y_test)
+    plt.plot(x_test, y_pred, color='red', linewidth = 3)
+    plt.title("Regresión Lineal Simple")
+    plt.xlabel('Numero de habitaciones')
+    plt.ylabel('Valore medio')
+    plt.show()
+
+    input("Enter para ver la precision del modelo")
+    print()
+    print('precision:')
+    print(lr.score(x_train, y_train))
+
+
+
+if __name__ == '__main__':
+    Init()
