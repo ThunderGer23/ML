8c972d8ff2f685f6e859a662b9831375d8b6b9bd
ThunderGer23 <Cuando2son1@hotmail.com>
2021-01-18 02:58:34 -0600

    Cambio de nombre y agregando teoria

---
+++ Teoria de introducción/Regresión lineal.py
@@ -0,0 +1,71 @@
+'''
+                    Qué es la Varianza?
+
+En teoria de probabilidad, la varianza de una variable aleatoria
+es una medida de dispersión definida como la esperanza del
+cuadrado de la dessviacion de dicha variable respecto a su medida.
+
+Hay que tener en cuenta que la varianza puede verse muy influida
+por los valores atipicos y no se aconseja su uso cuando las
+distribuciones de las variables aleatorias tiene colas pesadas.
+
+
+                    Qué es una Bia/Sesgo?
+
+El sesgo es un peso desproporcionado a favor o en contra de una cosa,
+persona o grupo en comparación con otra, generalmente de una manera
+que se considera injusta.
+
+Los sesgos pueden aprender observando diferentes contextos culturales.
+Las personas pueden desarrollar sesgos hacia o en contra de un individuo,
+un grupo étnico, una identidad sexual o de género, una nacion o religión.
+
+En ciencia e ingenieria, un sesgo es un error sistematico. El sesgo
+estadistico resulta de un muestreo injusto de una población, o de un
+proceso de estimacion que no da resultados precisos en promedio.
+
+
+            Tipos de errores presentes en la programacion de una IA
+
+Existen tres tipos de errotes que pueden presentarse en nuestro sistema:
+
+        Error de Bias:
+            Esta es la diferencia entre la predicción esperada de nuestro
+            modelo y los valores verdaderos que hemos conseguido durante la
+            ejecución.
+
+            Los algoritmos paramétricos tienen un alto Bias que los hace
+            más rápidos de aprender y más fácil de entender, pero que
+            generalmente también los vuelve menos flexibles y un menor
+            rendimiento predictivo en problemas más complejos.
+
+        Error de Varianza:
+
+            Este se refiere a la cantidad que la estimación de la función
+            cambiara si se utiliza diferentes datos de entrenamiento, este
+            se estima a partir de los datos obtenidos para la programacion
+            de nuestro algoritmo de ML.
+
+            Idealmente no deberia cambiar demasiado de un conjunto de datos
+            de entrenamiento a otro.
+
+        Error irreducible:
+            Este ultimo no se puede evitar independientemente del algoritmo
+            que se utilice, también se le nombra ruido y por lo general
+            proviene de variables desconocidas que interfieren en el mapeo de
+            las variables de entrada o de salida.
+
+            También puede provenir de Caracteristicas incompletas en nuestro
+            sistema o un problema no detectado dentro del mismo, cabe resaltar
+            que no importa cúan bueno sea el modelo que hemos elaborado, todos
+            nuestros datos tendran cierta cantidad de ruido o error irreducible.
+
+
+                                Compensación BIAS-VARIANZA
+        El objetivo del ML es lograr un bias bajo y una varianza baja, a su vez,
+        el algoritmo debe lograr un buen rendimiento de predicción.
+
+        El bias frente a la varianza se refiere a la precision frente a la consistencia
+        de los modelos entrenados por su algoritmo.
+'''
+
